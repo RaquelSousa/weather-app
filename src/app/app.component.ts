13f49a44ef50138b686573065e169f5b73854e14
RaquelSousa <raquel.sousa.wt@gmail.com>
2024-02-29 10:55:51 +0000

    first weather app commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WeatherComponent } from './weather/weather.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, WeatherComponent],
 })
 export class AppComponent {
   title = 'weatherapp';
